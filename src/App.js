c89b7ce3b4e9e3370cb2900dfbca5f357b43ec94
raneemmagdy <raneemmagdy2002@gmail.com>
2024-09-19 22:27:54 +0300

    page commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import About from './About/About'
 import Layout from './Layout'
 import Contact from './ContactUs/Contact'
 import Notfound from './Notfound/Notfound'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createHashRouter } from 'react-router-dom'
 import Portfolio from './Portfolio/Portfolio'
 import Start from './Start/Start'
 
 export default function App() {
 
-  const routes=createBrowserRouter([
+  const routes=createHashRouter([
     {path:'',element:<Layout></Layout>,children:[
       {index:true,element:<Start></Start>},
       {path:'about',element:<About></About>},
